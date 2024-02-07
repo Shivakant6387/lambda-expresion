c643665b518138fcc0331ab34d93a34d59c29a37
Shivakant6387 <shivakantsingh900@gmail.com>
2024-02-07 17:37:29 +0530

    with out use brases

--- src/main/java/org/example/stream/HelloWorldLambda.java
+++ src/main/java/org/example/stream/HelloWorldLambda.java
@@ -2,9 +2,7 @@ package org.example.stream;
 
 public class HelloWorldLambda {
     public static void main(String[] args) {
-        HelloWorldInterface helloWorldInterface=()->{
-            return "Hello World!";
-        };
+        HelloWorldInterface helloWorldInterface=()-> "Hello World!";
         System.out.println(helloWorldInterface.sayHelloWorld());
     }
 }
